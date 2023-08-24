3941b00176098d9e63a873aef220002bc4e4aea4
William Zapata <55813562+Wize6669@users.noreply.github.com>
2023-08-24 18:38:39 -0500

    Update Navbar

--- Examen-ZAPATA-B2/crud_restaurant_dish/components/Navbar.tsx
+++ Examen-ZAPATA-B2/crud_restaurant_dish/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav className={"flex justify-between items-center bg-slate-700 px-8 py-3"}>
             <Link className={"text-white font-bold"} href={"/"}>CRUD R&D.</Link>
-            <Link className={"bg-white p-2"} href={"/AddRestaurant"}>Add Restaurant</Link>
+            <Link className={"bg-white p-2"} href={"/addRestaurant"}>Add Restaurant</Link>
         </nav>
     );
 }

b702dfc7704f0ffc4fb2c3098e658ec5ba1ebcff
William Zapata <55813562+Wize6669@users.noreply.github.com>
2023-09-05 21:44:28 -0500

    Update

--- Examen-ZAPATA-B2/crud_restaurant_dish/components/RemoveBtn.tsx
+++ Examen-ZAPATA-B2/crud_restaurant_dish/components/RemoveBtn.tsx
@@ -3,27 +3,43 @@
 import React from "react";
 import { HiOutlineTrash } from "react-icons/hi";
 import { useRouter } from "next/navigation";
+import { Toaster, toast } from "sonner";
+import { BsFillTrashFill } from "react-icons/bs";
 
 export default function RemoveBtn({ idRestaurant }) {
     const router = useRouter();
 
     const removeRestaurant = async () => {
-        const confirmed = confirm('Are you sure?');
+        toast('Are you sure?', {
+            icon: <BsFillTrashFill style={{ color: "red" }} />,
+            action: {
+                label: 'Delete',
+                onClick: async () => {
+                    const res = await fetch(`api/restaurants?idRestaurant=${idRestaurant}`, {
+                        method: "DELETE",
+                    });
 
-        if (confirmed) {
-            const res = await fetch(`api/restaurants?idRestaurant=${idRestaurant}`, {
-                method: "DELETE",
-            });
+                    if (res.ok) {
+                        toast.success('Restaurant removed', { style: { fontSize: "1.5rem" } });
+                        router.refresh();
+                    } else {
+                        toast.error('Error deleted', { style: { fontSize: "1.5rem" } });
+                        console.log("Error deleted");
+                    }
+                },
+            }, style: { fontSize: "1.5rem" }
+        });
 
-            if (res.ok) {
-                router.refresh();
-            } else {
-                console.log("Error deleted")
-            }
-        }
+
     };
 
-    return <button onClick={removeRestaurant} className={"text-red-100"}>
-        <HiOutlineTrash size={"24"} />
-    </button>;
+    return (
+        <div>
+            <button onClick={removeRestaurant} className={"text-red-100"}>
+                <HiOutlineTrash size={"24"} />
+            </button>
+
+            <Toaster position="top-center" closeButton />
+        </div>
+    );
 }
--- Examen-ZAPATA-B2/crud_restaurant_dish/components/RemoveBtnDish.tsx
+++ Examen-ZAPATA-B2/crud_restaurant_dish/components/RemoveBtnDish.tsx
@@ -3,27 +3,41 @@
 import { HiOutlineTrash } from "react-icons/hi";
 import React from "react";
 import { useRouter } from "next/navigation";
+import { Toaster, toast } from "sonner";
+import { BsFillTrashFill } from "react-icons/bs";
 
 export default function RemoveBtnDish({ idDish }) {
     const router = useRouter();
 
     const removeRestaurant = async () => {
-        const confirmed = confirm('Are you sure?');
+        toast('Are you sure?', {
+            icon: <BsFillTrashFill style={{ color: "red" }} />,
+            action: {
+                label: 'Delete',
+                onClick: async () => {
+                    const res = await fetch(`/api/dishes?idDish=${idDish}`, {
+                        method: "DELETE",
+                    });
 
-        if (confirmed) {
-            const res = await fetch(`/api/dishes?idDish=${idDish}`, {
-                method: "DELETE",
-            });
-
-            if (res.ok) {
-                router.refresh();
-            } else {
-                console.log("Error deleted")
-            }
-        }
+                    if (res.ok) {
+                        toast.success('Dish removed', { style: { fontSize: "1.5rem" } });
+                        router.refresh();
+                    } else {
+                        toast.error('Error deleted', { style: { fontSize: "1.5rem" } });
+                        console.log("Error deleted");
+                    }
+                },
+            }, style: { fontSize: "1.5rem" }
+        });
     };
 
-    return <button onClick={removeRestaurant} className={"text-red-100"}>
-        <HiOutlineTrash size={"24"} />
-    </button>;
+    return (
+        <div>
+            <button onClick={removeRestaurant} className={"text-red-100"}>
+                <HiOutlineTrash size={"24"} />
+            </button>
+
+            <Toaster position="top-center" closeButton />
+        </div>
+    );
 }
